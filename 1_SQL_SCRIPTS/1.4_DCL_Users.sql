9268b2a6c5f4b25bf95f8d7d38b2391ab9f03f23
Kurt Dela Cruz <bbpdimawala@gmail.com>
2025-12-15 22:38:43 +0800

    Update 1.4_DCL_Users.sql

--- 1_SQL_SCRIPTS/1.4_DCL_Users.sql
+++ 1_SQL_SCRIPTS/1.4_DCL_Users.sql
@@ -1,1 +1,6 @@
 
+USE SDG2_ZeroHungerDB;
+
+CREATE USER IF NOT EXISTS 'foodbank_admin'@'localhost' IDENTIFIED BY 'SecurePass123!';
+GRANT SELECT, INSERT, UPDATE ON SDG2_ZeroHungerDB.* TO 'foodbank_admin'@'localhost';
+FLUSH PRIVILEGES;
